2c09b416be287ed943220ee6ae71e9b958a368b7
TylerWhLab <tylerdurden.wh@gmail.com>
2023-08-17 16:28:28 +0900

    2013-08-17
    add APIs
    
    Signed-off-by: TylerWhLab <tylerdurden.wh@gmail.com>

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 """
 author: 김정균
-sudo apt install mosquitto
 """
 
 import socket
@@ -10,6 +9,8 @@ import struct
 import os
 import paho.mqtt.client as mqtt
 import threading
+import json
+from queue import Queue
 from time import sleep
 
 
@@ -17,14 +18,13 @@ ServerIP = '10.10.14.3'
 resData = -1
 
 
-def run_streaming():
+def run_streaming(Port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((ServerIP, 9876))
+    client_socket.connect((ServerIP, Port))
     
     ### Start cv
-    #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("/home/intel/repo/kcci.intel.ai.project/Class02/smart_factory_src/resources/factory/conveyor.mp4")
-    # fcnt = 0
+    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture("/home/intel/repo/kcci.intel.ai.project/Class02/smart_factory_src/resources/factory/conveyor.mp4")
     while True:
         _, frame = cap.read()
         if frame is None:
@@ -34,15 +34,13 @@ def run_streaming():
         message = struct.pack("Q", len(serialFrame)) + serialFrame  # 직렬화된 frame의 길이(Q==8) + 직렬화된 data => recv할때 해당 길이만큼 데이터 역직렬화
         client_socket.sendall(message)  # frame(한 장) byte로 전송
 
-        cv2.imshow('TRANSMITTING VIDEO', frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-        # fcnt += 1
-        # print(f'{fcnt}. len(message) : {len(message)}')
+        # cv2.imshow('TRANSMITTING VIDEO', frame)
+        # if cv2.waitKey(10) & 0xFF == ord('q'):
+        #     break
     # End while
 
     client_socket.close()
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
     cap.release()
     ### End cv
     
@@ -50,7 +48,7 @@ def run_streaming():
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    print("Connected with result code " + str(rc))  # 0이면 정상 연결
 
 # End def
 
@@ -66,6 +64,10 @@ def on_message(client, userdata, message):
     resData = str(message.payload.decode("utf-8"))
 
     print(f'mqtt mag : {resData}')
+
+    ### TODO
+    ### 메시지에 따른 대응
+
 # End def
 
 
@@ -74,30 +76,72 @@ def mqtt_sub():
     mqtt_sub.on_connect = on_connect
     mqtt_sub.on_message = on_message
     mqtt_sub.subscribe('vigilante/action')
-    # try:
     mqtt_sub.connect(ServerIP, 1888)
-    # except Exception as e:
-        # print(f'except@@@@@@@@@@@@@@@@@@@@@ : {e}') # [Errno 111] Connection refused
     
     print('wait mqtt msg')
     mqtt_sub.loop_forever()
 # End def
 
 
+def info_send(Port, msg_queue):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ServerIP, Port))
+    req_data = dict()
+
+    while True:
+        # TODO : GPS 모듈로 값 받기
+
+        gps = '37.5413, 126.841'  # KFC
+        try:
+            event_yn = msg_queue.get_nowait()  # from  def ai_func(msg_queue):
+        except:
+            continue
+
+        req_data['gps'] = gps
+        req_data['event_yn'] = event_yn
+
+        sleep(1)
+        client_socket.socket.send(json.dumps(req_data).encode('UTF-8'))
+    # End while
+
+    client_socket.close()
+# End def
+
+
+def ai_func(msg_queue):
+    while True:
+        ### TODO
+        ### 사건 발생 유무 받아서 event_yn에 넣기
+        event_yn = False
+        msg_queue.put(event_yn)
+    # End while
+# End def    
+
+
 def main():
     # run_streaming()
     # mqtt_sub()
 
-    t1 = threading.Thread(target=run_streaming, args=(), daemon=True)
+    msg_queue = Queue()
+
+    t1 = threading.Thread(target=run_streaming, args=(9876, ), daemon=True)
     t2 = threading.Thread(target=mqtt_sub, args=(), daemon=True)
+    t3 = threading.Thread(target=info_send, args=(9777, msg_queue), daemon=True)
+    # t_ai = threading.Thread(target=ai_func, args=(msg_queue, ), daemon=True)
     
     t1.start()
     print("run_streaming start")
     t2.start()
     print("mqtt_sub start")
+    t3.start()
+    print("info_send start")
+    # t_ai.start()
+    # print("t_ai start")
 
     t1.join()
     t2.join()
+    t3.join()
+    # t_ai.join()
 
     
 # End def    
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 """
 author: 김정균
+sudo apt install libmariadb-dev
+sudo apt install mariadb-server mariadb-client # systemctl status mariadb.service 
+sudo apt install mosquitto
 """
 
 import socket
@@ -7,17 +10,23 @@ import cv2
 import pickle
 import struct
 import os
+import sys
+import threading
+import mariadb
+import json
+from queue import Queue
 
 
-def run_streaming():
-    ServerIP = '10.10.14.3'
-    Port = 9876
+ServerIP = '10.10.14.3'
+Event = False
 
+
+def run_streaming(Port, msg_queue):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 즉시 재사용
     server_socket.bind((ServerIP, Port))
     server_socket.listen(5)
-    print(f'LISTENING AT : {ServerIP}:{Port}')
+    print(f'\nStreaming(Filed Client) LISTENING AT : {ServerIP}:{Port}')
 
     data = b""
     payload_size = struct.calcsize("Q")  # 8byte
@@ -54,13 +63,44 @@ def run_streaming():
                 frame_data = data[:msg_size]  # 길이 정보 제거한 data 부분
                 data = data[msg_size:]  # data 초기화
                 frame = pickle.loads(frame_data)  # 역직렬화(byte sequence를 frame 객체로 변환)
+                
+                if Event == True:
+                    # 동영상 저장 설정
+                    from datetime import datetime
+                    now = datetime.now()
 
-                cv2.imshow("RECEIVING VIDEO", frame)
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
+                    output_filename = f'{now.date()}_{now.time()}.mp4'  # 저장할 동영상 파일 이름
+                    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 코덱 설정 (XVID는 AVI 형식)
+                    fps = 30.0  # 초당 프레임 수
+                    frame_size = (640, 480)  # 프레임 크기
+
+                    # VideoWriter 객체 생성
+                    out = cv2.VideoWriter(output_filename, fourcc, fps, frame_size)
+                    
+                    # 프레임 저장
+                    out.write(frame)
+                    
+                    # 화면에 프레임 출력
+                    # cv2.imshow('Video Recording', frame)
+
+                    # 리소스 해제
+                    out.release()
+                    # cv2.destroyAllWindows()
+                # End if
+
+
+                # 직렬화된 frame data => frame_data
+                serialFrame = frame_data
+                message = struct.pack("Q", len(serialFrame)) + serialFrame  # 직렬화된 frame의 길이(Q==8) + 직렬화된 data => recv할때 해당 길이만큼 데이터 역직렬화
+                msg_queue.put(message)
+                # client_socket.sendall(message)  # frame(한 장) byte로 전송
+
+                # cv2.imshow("RECEIVING VIDEO", frame)
+                # if cv2.waitKey(10) & 0xFF == ord('q'):
+                #     break
             # End while
 
-            cv2.destroyAllWindows()
+            # cv2.destroyAllWindows()
             client_socket.close()
 
         # End if
@@ -68,21 +108,179 @@ def run_streaming():
 # End def
 
 
+def run_mqtt():
+    os.system('mosquitto -p 1888')
+# End def    
+
+
+def send_streaming(Port, msg_queue):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 즉시 재사용
+    server_socket.bind((ServerIP, Port))
+    server_socket.listen(5)
+    print(f'\nStreaming(QTClient) LISTENING AT : {ServerIP}:{Port}')
+
+    while True:
+        print('waiting accept...')
+        client_socket, addr = server_socket.accept()
+        print('GOT CONNECTION FROM:', addr)
+
+        if client_socket:
+
+            # serialize frame 그대로 전송
+            while True:
+                try:
+                    serialFrame = msg_queue.get_nowait()
+                except:
+                    continue
+                client_socket.sendall(serialFrame)  # frame(한 장) byte로 전송
+
+            client_socket.close()
+        # End if
+    # End while
+# End def    
+
+
+# DB
+def client_db_req(Port, cur):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 즉시 재사용
+    server_socket.bind((ServerIP, Port))
+    server_socket.listen(5)
+    print(f'\nDB Request LISTENING AT : {ServerIP}:{Port}')
+
+    while True:
+        print('waiting accept...')
+        client_socket, addr = server_socket.accept()
+        print('GOT CONNECTION FROM:', addr)
+
+        if client_socket:
+            cur.execute(
+                "SELECT * \
+                   FROM TB_DDUKNIP \
+                  WHERE MACHINE_NAME=? \
+                  ORDER BY EVENT_DATE DESC \
+                  LIMIT 1"
+                , ('TurtleBot',)
+            )
+            
+            # Print Result-set
+            for (VIDEO_NAME, GPS_COORDINATE) in cur:
+                print(f"VIDEO_NAME: {VIDEO_NAME}, GPS_COORDINATE: {GPS_COORDINATE}")
+
+
+            client_socket.send(cur.encode('utf-8'))
+        # End if
+
+
+            #client_socket.close()
+        # End if
+    # End while
+# End def   
+
+
+def recv_from_field(Port, cur, conn):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 즉시 재사용
+    server_socket.bind((ServerIP, Port))
+    server_socket.listen(5)
+    print(f'\nrecv_from_field LISTENING AT : {ServerIP}:{Port}')
+
+    while True:
+        print('waiting accept...')
+        client_socket, addr = server_socket.accept()
+        print('GOT CONNECTION FROM:', addr)
+
+        if client_socket:
+            while True:
+
+                recvData = client_socket.recv(1024).decode('utf-8')
+                if not recvData:
+                    break
+                print(f"수신한 데이터: {type(recvData)}")
+
+                dictData = json.loads(recvData)
+                print(dictData)
+
+                global Event
+                if dictData['event_yn'] == True:
+                    # 영상 저장
+                    Event = True
+                else:
+                    Event = False
+
+
+                # INSERT
+                try: 
+                    cur.execute("INSERT INTO TB_DDUKNIP ( \
+                                    MACHINE_NAME \
+                                    VIDEO_NAME \
+                                    GPS_COORDINATE \
+                                    EVENT_YN \
+                                ) VALUES \
+                                    (?, ?, ?, ?, ?)"
+                                , 
+                                ("TurtleBot"
+                                 , path
+                                 , dictData['gps']
+                                 , dictData['event_yn']
+                                 )) 
+                except mariadb.Error as e: 
+                    print(f"Error: {e}")
+
+                conn.commit() 
+                print(f"Last Inserted ID: {cur.lastrowid}")
+
+
+            # End while
+        # End if
+    # End while
+# End def
+
+
 def main():
-    run_streaming()
+    # run_streaming()
+    # client_req()
 
+    msg_queue = Queue()
 
-    # t1 = threading.Thread(target=def1, args=(,), daemon=True)
-    # t2 = threading.Thread(target=def2, args=(,), daemon=True)
+    # Connect to MariaDB Platform
+    try:
+        conn = mariadb.connect(
+            user="user1",
+            password="user1",
+            host="127.0.0.1",
+            port=3306,
+            database="TB_DDUKNIP"
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
+
+    run_mqtt()
+    t1 = threading.Thread(target=run_streaming, args=(9876, msg_queue, ), daemon=True)
+    t2 = threading.Thread(target=send_streaming, args=(9888, msg_queue, ), daemon=True)
+    t3 = threading.Thread(target=client_db_req, args=(9899, cur), daemon=True)
+    t4 = threading.Thread(target=recv_from_field, args=(9777, cur, conn), daemon=True)  # GPS, 상황발생YN 받기
     
-    # t1.start()
-    # print("def1 start")
-    # t2.start()
-    # print("def2 start")
+    t1.start()
+    print("run_streaming start")
+    t2.start()
+    print("send_streaming start")
+    t3.start()
+    print("client_db_req start")
+    t4.start()
+    print("recv_from_field start")
 
-    # t1.join()
-    # t2.join()
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
 
+    conn.close()
 # End def
 
 
